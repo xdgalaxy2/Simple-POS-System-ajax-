a40548659437e1621c5b7a32579021766250f3b2
User_20200273 <allen.pagnanawon@foundationu.com>
2023-07-27 18:45:14 +0800

    catering mysql

--- mysql-connect.php
+++ mysql-connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "ITS4";
+$database = "catering";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
