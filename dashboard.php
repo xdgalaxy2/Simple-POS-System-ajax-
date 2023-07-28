0b20ecb39973871d02918cfab8c6c9951c51aa67
User_20200273 <allen.pagnanawon@foundationu.com>
2023-07-28 17:58:47 +0800

    order list

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,7 @@
                 <thead>
                     <tr>
                         <br>              
-                        <td scope="col">ID</td><td scope="col">date_order</td><td scope="col">deleviry_date</td><td scope="col">delivery_address</td><td scope="col">delivery_time</td><td scope="col">contact_number</td>
+                        <td scope="col">ID</td><td scope="col">Date Order</td><td scope="col">Deleviry Date</td><td scope="col">Delivery Address</td><td scope="col">Delivery Time</td><td scope="col">Contact Number</td>
                     </tr>
                 </thead>
                 <!-- List Loop -->
