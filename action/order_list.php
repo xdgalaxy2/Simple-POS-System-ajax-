5c2cec957c1d87ae691e0295b30808918a727e21
User_20200273 <allen.pagnanawon@foundationu.com>
2023-07-28 18:17:01 +0800

    order list

--- action/order_list.php
+++ action/order_list.php
@@ -1,15 +1,4 @@
 <?php
-session_start();
-error_reporting(0);
-require '../mysql-connect.php';
-
-  /*
-  if($_POST['search']){
-      $where = " AND ( firstname LIKE '%".$_POST['search']."%'  OR date_order LIKE '%".$_POST['search']."%' OR id LIKE '%".$_POST['search']."%' )";
-  }else{
-    $where="";
-  }
-  */
 
 $sql = "SELECT * FROM orders WHERE id != ".$_SESSION['user_id']." $where ORDER BY date_order";
 
@@ -27,11 +16,6 @@ if ($result->num_rows > 0) {
         <td scope="row"><?php echo $row[' delivery_address']; ?></td>
         <td scope="row"><?php echo $row[' contact_number']; ?></td>
         <td scope="row">
-          
-          <a href="#" class="delete-user dropdown-item text-danger small" data-id="<?php echo $row['id']; ?>">DELETE</a> 
-
-        </div>
-        </div>
         </td>
     </tr>
     <?php
