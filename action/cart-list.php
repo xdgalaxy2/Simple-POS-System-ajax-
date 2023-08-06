c254723ef036f1a1d0efbd42ef173603bcd43091
User_20200273 <allen.pagnanawon@foundationu.com>
2023-08-07 02:13:10 +0800

    fixed added quantity

--- action/cart-list.php
+++ action/cart-list.php
@@ -3,41 +3,36 @@ session_start();
 require '../mysql-connect.php';
 
 $totalamount = 0;
-if (isset($_SESSION['orders']) && is_array($_SESSION['orders'])) {
-    foreach ($_SESSION['orders'] as $key => $menu_id) {
-        $sql = "SELECT * FROM menu WHERE id =" . $menu_id;
 
+if (isset($_SESSION['orders']) && is_array($_SESSION['orders'])) {
+    // Count the occurrences of each menu item in the $_SESSION['orders'] array
+    $menuQuantities = array_count_values($_SESSION['orders']);
+
+    foreach ($menuQuantities as $menu_id => $quantity) {
+        $sql = "SELECT * FROM menu WHERE id = " . $menu_id;
         $result = $conn->query($sql);
 
-        
         // output data of each row
         if ($row = $result->fetch_assoc()) {
-            $quantity = 1;
             $total = $row['price'] * $quantity;
-            $totalamount = $total + $totalamount;
+            $totalamount += $total;
+            ?>
 
-        ?>
+            <tr>
+                <td scope="row"><?php echo $row['description']; ?></td>
+                <td scope="row"><?php echo $row['price']; ?></td>
+                <td scope="row"><?php echo $quantity; ?></td>
+                <td scope="row"><?php echo number_format($total, 2); ?></td>
+            </tr>
 
-        <tr>
-            <td scope="row"><?php echo $row['description']; ?></td>
-            <td scope="row"><?php echo $row['price']; ?></td>
-            <td scope="row"><?php echo $quantity; ?></td>
-            <td scope="row"><?php echo number_format($total, 2); ?></td>
-        </tr>
-
-        <?php
-                    }
-
-            }
-        ?>
-        <tr>
-            <td colspan ="3">Total Amount:</td>
-            <td scope="row"><?php echo number_format($totalamount ,2); ?></td>
-        </tr>
-        <?php
+            <?php
         }
-    
+    }
+    ?>
+    <tr>
+        <td colspan="3">Total Amount:</td>
+        <td scope="row"><?php echo number_format($totalamount, 2); ?></td>
+    </tr>
+    <?php
+}
 ?>
-
-
-                              
